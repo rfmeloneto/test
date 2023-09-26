eb067200102a40f208d9d40730d2ec4b0434fda8
Raimundo <raimundo@mpto.mp.br>
2023-09-25 21:40:00 -0300

    fix

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import json
+import requests
+import os
+import ast
 
 import dash
 import dash_bootstrap_components as dbc
@@ -8,9 +11,11 @@ import dash_ag_grid as dag
 
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from dotenv import load_dotenv
 
 from pages.page_candidatos import candidatos
 from pages.bus import bus
+
 
 app = dash.Dash(__name__, external_stylesheets=[
                 dbc.themes.FLATLY], suppress_callback_exceptions=True)
@@ -18,17 +23,51 @@ app = dash.Dash(__name__, external_stylesheets=[
 app.title = 'IntegraVoto'
 app._favicon = "asserts/favicon.ico"
 
-df = pd.read_csv("urnas.csv")
-dft = pd.read_csv("resultado.csv")
-df_brancos_nulos = pd.read_csv("brancos_nulos.csv")
+load_dotenv()
+
+# DataSource dos Pleitos - Carrega os inputs de Estado, Cidade e Região
+url_pleitos = os.getenv("PLEITOS_ATIVOS")
+pleitos = requests.get(url_pleitos).json()
+df_pleitos = pd.DataFrame(pleitos)
+
+# DataSource dos Candidatos - Carrega as informações dos Resultados
+
+
 filejson = open("geodata/to.json")
 geojson = json.load(filejson)
 
 total_circulos = 150
-circulos_amarelos = 28
+circulos_amarelos = 100
 
 components = [candidatos(), bus(
     total_circulos, circulos_amarelos)]
+
+url_resultado = os.getenv("RESULTADOS")+"1/"
+print(url_resultado)
+
+
+def update_table(cidade, regiao):
+    url_resultado = os.getenv("RESULTADOS")+str(cidade)+"/"
+    resultato = requests.get(url_resultado).json()
+    dft = pd.DataFrame(resultato)
+    df_city = dft[(dft["cidade"] == "Palmas") & (dft["regiao"] == regiao)].sort_values(
+        by="votos", ascending=False)
+    colocados = [i+1 for i in range(0, len(df_city["candidato"].unique()))]
+    df_city['colocacao'] = colocados
+    return dag.AgGrid(
+        columnDefs=[
+            {"headerName": "Posição", "field": "colocacao"},
+            {"headerName": "Nome", "field": "nome_candidato"},
+            {"headerName": "Número", "field": "candidato"},
+            {"headerName": "Votos", "field": "votos"},
+        ],
+        rowData=df_city.to_dict(orient="records"),
+        columnSize="sizeToFit",
+        style={"height": "360px", "width": "100%", "justify": "center"},
+
+
+
+    )
 
 
 app.layout = html.Div(
@@ -71,36 +110,25 @@ app.layout = html.Div(
                 dcc.Dropdown(
                     id="dropdown-estado",
                     options=[
-                        {"label": "Tocantins", "value": "TO"},
-                        {"label": "Pernabuco", "value": "PE"},
-                        {"label": "Rio Grande do Norte", "value": "RN"},
+                        {"label": estado, "value": uf} for estado, uf in zip(df_pleitos["estado"].unique(), df_pleitos["uf"].unique())
                     ],
 
                     className="p-4",
                     value="TO",),
             ]),
             dbc.Col([
-                dcc.Dropdown(
-                    id="dropdown-cidade",
-                    options=[
-                        {"label": cidade, "value": cidade}
-                        for cidade in df["cidade"].unique()
-                    ],
-                    value="Palmas",
-                    className="p-4",
-                ),
-            ]),
+                    dcc.Dropdown(
+                        id="dropdown-cidade",
+                        placeholder="Cidade",
+                        className="p-4",
+                    ),
+                    ]),
 
             dbc.Col([
+
                 dcc.Dropdown(
                     id="dropdown-regiao",
-                    options=[
-                        {"label": "Única", "value": 1},
-                        {"label": "Sul", "value": 2},
-                        {"label": "Norte", "value": 3},
-                    ],
                     placeholder="Região",
-                    value="Única",
                     className="p-4",
                 ),
             ]),
@@ -172,21 +200,30 @@ app.layout = html.Div(
 )
 
 
+# Atualização das cidades de acordo com o estado
 @app.callback(
-    Output('table', 'data'),
-    Input('cidade-dropdown', 'value')
+    Output('dropdown-cidade', 'options'),
+    Input('dropdown-estado', 'value')
 )
-def update_table(cidade):
-    # Filtra o DataFrame com base na cidade selecionada
-    filtered_df = df[df['Cidade'] == cidade]
+def load_city(uf):
+    cidades = df_pleitos[df_pleitos['uf'] == uf]['cidade'].unique()
+    id = df_pleitos[df_pleitos['uf'] == uf]['id'].unique()
+    options = [{"label": cidades[i], "value": id[i]}
+               for i in range(len(cidades))]
+    return options
 
-    # Remove as colunas dos candidatos que não têm votos na cidade filtrada
-    filtered_df = filtered_df.loc[:, (filtered_df != 0).any()]
+# Atualização das regiões de acordo com a cidade
 
-    # Converte o DataFrame filtrado em uma lista de dicionários para uso no AgGrid
-    data = filtered_df.to_dict('records')
 
-    return data
+@app.callback(
+    Output('dropdown-regiao', 'options'),
+    Input('dropdown-cidade', 'value')
+)
+def load_regiao(cidade):
+    regioes = df_pleitos[df_pleitos['id'] == cidade]['regiao']
+    regiao = ast.literal_eval(regioes[0])
+    options = [{"label": r, "value": r} for r in regiao]
+    return options
 
 
 @app.callback(Output("components", "children"), Input("radio", "value"))
@@ -245,7 +282,7 @@ def update_nome_cidade(value):
     Input("dropdown-cidade", "value"),
 )
 def update_votos_brancos(value):
-    return (df_brancos_nulos[df_brancos_nulos["cidade"] == value]["votos_brancos"].sum(),)
+    return (df_brancos_nulos[df_brancos_nulos["cidade"] == value]["brancos"].sum(),)
 
 
 @app.callback(
@@ -253,7 +290,7 @@ def update_votos_brancos(value):
     Input("dropdown-cidade", "value"),
 )
 def update_votos_nulos(value):
-    return (df_brancos_nulos[df_brancos_nulos["cidade"] == value]["votos_nulos"].sum(),)
+    return (df_brancos_nulos[df_brancos_nulos["cidade"] == value]["nulos"].sum(),)
 
 
 @app.callback(
@@ -264,25 +301,9 @@ def update_votos_validos(value):
     return (dft[dft["cidade"] == value]["votos"].sum(),)
 
 
-@app.callback(Output("grid", "children"), Input("dropdown-cidade", "value"))
-def update_resultado(value):
-    df_city = dft[dft["cidade"] == value].sort_values(
-        by="votos", ascending=False)
-    colocados = [i+1 for i in range(0, len(df_city["candidato"].unique()))]
-    df_city['colocacao'] = colocados
-    return dag.AgGrid(
-        columnDefs=[
-            {"headerName": "Posição", "field": "colocacao"},
-            {"headerName": "Candidato", "field": "candidato"},
-            {"headerName": "Votos", "field": "votos"},
-        ],
-        rowData=df_city.to_dict(orient="records"),
-        columnSize="sizeToFit",
-        style={"height": "360px", "width": "100%", "justify": "center"},
-
-
-
-    )
+@app.callback(Output("grid", "children"), Input("dropdown-cidade", "value"), Input("dropdown-regiao", "value"))
+def update_resultado(cidade, regiao):
+    return update_table(cidade, regiao)
 
 
 if __name__ == "__main__":
